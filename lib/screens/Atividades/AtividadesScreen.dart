bb5524701f28ccc67d2bf4dd70af67bcf1941047
MatheusFinatto <matheusfinatto2@gmail.com>
2023-09-12 18:16:56 -0300

    feat: added appbar to make a default layout

--- lib/screens/Atividades/AtividadesScreen.dart
+++ lib/screens/Atividades/AtividadesScreen.dart
@@ -4,8 +4,18 @@ class AtividadesScreen extends StatelessWidget {
   const AtividadesScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Atividades Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Atividades'),
+      ),
+      body: Column(
+        children: [
+          // Search input field
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/screens/Conta/ContaScreen.dart
+++ lib/screens/Conta/ContaScreen.dart
@@ -4,8 +4,18 @@ class ContaScreen extends StatelessWidget {
   const ContaScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Conta Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Conta'),
+      ),
+      body: Column(
+        children: [
+          // Search input field
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+          ),
+        ],
+      ),
     );
   }
 }
