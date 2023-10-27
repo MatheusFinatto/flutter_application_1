537308fa1981dc048f2dddf8fa527f3d70b19180
MatheusFinatto <matheusfinatto2@gmail.com>
2023-10-27 11:49:40 -0300

    feat: added firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screens/home/home_page.dart';
-import 'package:flutter_application_1/screens/splash_screen/splash_screen.dart';
+import 'package:flutter_application_1/screens/home/splash_screen/splash_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MainApp());
 }
 
